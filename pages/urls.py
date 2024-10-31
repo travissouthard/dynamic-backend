1b075836eac57dc18fe8a278a9e15ef798fb82e4
travissouthard <tsouthard88@gmail.com>
2024-10-30 23:27:01 -0400

    Add existing list routes on website

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from .views import homeView, aboutView
+from .views import home_view, about_view, projects_view, art_view, blog_view, webring_view, resume_view, rss_view, redirect_old_html_links
 
 urlpatterns = [
-    path("", homeView, name="home"),
-    path("about", aboutView, name="about")
+    path("<str:route>.html", redirect_old_html_links, name="html_links"),
+    path("", home_view, name="home"),
+    path("about", about_view, name="about"),
+    path("resume", resume_view, name="resume"),
+    path("webring", webring_view, name="webring"),
+    path("rss", rss_view, name="rss"),
+    path("rss.xml", rss_view, name="rss"),
+    path("projects", projects_view, name="projects"),
+    path("art", art_view, name="art"),
+    path("blog", blog_view, name="blog")
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,45 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-def homeView(request):
+def redirect_old_html_links(_, route):
+    return HttpResponseRedirect(route)
+
+def home_view(request):
     template = loader.get_template("home.html")
     context = {"name": "Home"}
     return HttpResponse(template.render(context, request))
 
-def aboutView(request):
+def about_view(request):
     template = loader.get_template("about.html")
     context = {"name": "About"}
+    return HttpResponse(template.render(context, request))
+
+def projects_view(request):
+    template = loader.get_template("projects.html")
+    context = {"name": "Projects"}
+    return HttpResponse(template.render(context, request))
+
+def art_view(request):
+    template = loader.get_template("art.html")
+    context = {"name": "Art"}
+    return HttpResponse(template.render(context, request))
+
+def blog_view(request):
+    template = loader.get_template("blog.html")
+    context = {"name": "Blog"}
+    return HttpResponse(template.render(context, request))
+
+def webring_view(request):
+    template = loader.get_template("webring.html")
+    context = {"name": "Webring"}
+    return HttpResponse(template.render(context, request))
+
+def resume_view(request):
+    template = loader.get_template("resume.html")
+    context = {"name": "Resume"}
+    return HttpResponse(template.render(context, request))
+
+def rss_view(request):
+    template = loader.get_template("rss.xml")
+    context = {}
     return HttpResponse(template.render(context, request))
